b6a9d7e756b6f37bace923180cbe4f1b4d35df8d
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2022-09-05 15:30:02 -0400

    testing mobile display functions

--- display.js
+++ display.js
@@ -1,6 +1,10 @@
 let header = document.getElementById('header');
 let container = document.getElementById('container');
 
+/Mobile/.test(navigator.userAgent) && !location.hash && setTimeout(function () {
+    if (!scrollY) window.scrollTo(0, 1);
+}, 1000);
+
 function hideHeader() {
     //keyframes
     container.animate([
